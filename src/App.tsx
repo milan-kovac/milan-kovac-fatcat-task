23649ef13a307cda4278bce769513fcfa1480718
Milan Kovac <113798238+milan-kovac@users.noreply.github.com>
2024-06-25 12:52:38 +0200

    Create a Form Generator Component

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import React from 'react';
 import './styles.css';
 
 import { Landing } from './components/landing/Landing';
+import PostForm from './components/PostForm';
 
 function App() {
     return (
         <main>
             <Landing />
+            <PostForm />
         </main>
     );
 }
